6e328803e46d5e1ee4a43c77bd800c73146325e3
nicolasbilic <nicolasbilic9@gmail.com>
2024-12-11 10:49:27 +0100

    second commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,7 +10,7 @@ defineProps<{
     <h3>
       You’ve successfully created a project with
       <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next??
     </h3>
   </div>
 </template>
@@ -33,6 +33,7 @@ h3 {
 }
 
 @media (min-width: 1024px) {
+
   .greetings h1,
   .greetings h3 {
     text-align: left;
