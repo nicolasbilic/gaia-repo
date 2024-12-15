8a23042df7d0c8bbc932bfc5d780cc0c3c621460
nicolasbilic <nicolasbilic9@gmail.com>
2024-12-15 18:27:36 +0100

    gallery showcase done

--- src/pages/UserHome.vue
+++ src/pages/UserHome.vue
@@ -3,7 +3,7 @@
 
 <template>
   <div>
-    <section class="hero-section">
+    <!-- <section class="hero-section">
       <div class="hero-projects-container">
         <div class="hero-projects">
           <div class="item">
@@ -88,115 +88,144 @@
           </div>
         </div>
       </div>
-    </section>
+    </section> -->
+
     <section class="projects-section">
-      <div class="projects-container">
-        <div class="projects">
-          <img
-            src="../assets/webp/fey.webp"
-            alt="Fey"
-            width="auto"
-            height="auto"
-          />
-          <img
-            src="../assets/webp/barde.webp"
-            alt="Barde"
-            width="auto"
-            height="auto"
-          />
-          <img
-            src="../assets/webp/warrior.webp"
-            alt="Warrior"
-            width="auto"
-            height="auto"
-          />
-          <img
-            src="../assets/webp/emissary.webp"
-            alt="Emissary"
-            width="auto"
-            height="auto"
-          />
-          <img
-            src="../assets/webp/mage.webp"
-            alt="Mage"
-            width="auto"
-            height="auto"
-          />
-          <img
-            src="../assets/webp/blacksmith.webp"
-            alt="Blacksmith"
-            width="auto"
-            height="auto"
-          />
-          <img
-            src="../assets/webp/priest.webp"
-            alt="Priest"
-            width="auto"
-            height="auto"
-          />
-          <img
-            src="../assets/webp/pirate-galaxy-2.webp"
-            alt="Female galactic pirate with a sword"
-            width="auto"
-            height="auto"
-          />
-          <img
-            src="../assets/webp/spectral-sailor.webp"
-            alt="Spectral sailor"
-            width="auto"
-            height="auto"
-          />
-          <img
-            src="../assets/webp/soulkeeper.webp"
-            alt="Soulkeeper"
-            width="auto"
-            height="auto"
-          />
-          <img
-            src="../assets/webp/celestial-architect.webp"
-            alt="Celestial architect"
-            width="auto"
-            height="auto"
-          />
+      <!--________ Project Showcase ________-->
+      <div class="project-showcase">
+        <div class="container bg-danger">
+          <h2>Project Showcase</h2>
+          <div
+            class="d-flex flex-column flex-lg-row justify-content-center align-items-center"
+          >
+            <div
+              class="img-box d-flex justify-content-center align-items-start"
+            >
+              <img
+                src="../assets/webp/desert-sorcerer.webp"
+                alt="Desert sorcerer"
+                width="400"
+                height="267"
+                loading="lazy"
+              />
+            </div>
+            <div
+              class="img-box d-flex justify-content-center align-items-start"
+            >
+              <img
+                src="../assets/webp/cosmic-artisan.webp"
+                alt="Cosmic artisan"
+                width="400"
+                height="267"
+                loading="lazy"
+              />
+            </div>
+            <div
+              class="img-box d-flex justify-content-center align-items-start"
+            >
+              <img
+                src="../assets/webp/soulkeeper-2.webp"
+                alt="Soulkeeper"
+                width="400"
+                height="267"
+                loading="lazy"
+              />
+            </div>
+            <div
+              class="img-box d-flex justify-content-center align-items-start"
+            >
+              <img
+                src="../assets/webp/ocean-priest.webp"
+                alt="Ocean priest"
+                width="400"
+                height="267"
+                loading="lazy"
+              />
+            </div>
+            <div
+              class="img-box d-flex justify-content-center align-items-start"
+            >
+              <img
+                src="../assets/webp/druid-2.webp"
+                alt="Druid of the rivers"
+                width="400"
+                height="267"
+                loading="lazy"
+              />
+            </div>
+          </div>
         </div>
       </div>
 
-      <!-- More projects -->
-      <div class="more-projects-container">
-        <div class="more-projects">
-          <div class="img-container">
+      <!--________ Project Gallery ________-->
+      <div class="project-gallery">
+        <div class="container bg-warning">
+          <div class="projects">
             <img
-              class="hey"
-              src="../assets/webp/spectral-sailor.webp"
-              alt="Spectral sailor"
+              src="../assets/webp/fey.webp"
+              alt="Fey"
+              width="auto"
+              height="auto"
             />
-          </div>
-          <div class="img-container">
             <img
-              class="hey"
-              src="../assets/webp/spectral-sailor.webp"
-              alt="Spectral sailor"
+              src="../assets/webp/barde.webp"
+              alt="Barde"
+              width="auto"
+              height="auto"
             />
-          </div>
-          <div class="img-container">
             <img
-              class="hey"
-              src="../assets/webp/spectral-sailor.webp"
-              alt="Spectral sailor"
+              src="../assets/webp/warrior.webp"
+              alt="Warrior"
+              width="auto"
+              height="auto"
             />
-          </div>
-          <div class="img-container">
             <img
-              class="hey"
-              src="../assets/webp/spectral-sailor.webp"
-              alt="Spectral sailor"
+              src="../assets/webp/emissary.webp"
+              alt="Emissary"
+              width="auto"
+              height="auto"
             />
-          </div>
-          <div class="img-container">
             <img
-              class="hey"
+              src="../assets/webp/mage.webp"
+              alt="Mage"
+              width="auto"
+              height="auto"
+            />
+            <img
+              src="../assets/webp/blacksmith.webp"
+              alt="Blacksmith"
+              width="auto"
+              height="auto"
+            />
+            <img
+              src="../assets/webp/priest.webp"
+              alt="Priest"
+              width="auto"
+              height="auto"
+            />
+            <img
+              src="../assets/webp/pirate-galaxy-2.webp"
+              alt="Female galactic pirate with a sword"
+              width="auto"
+              height="auto"
+            />
+            <img
               src="../assets/webp/spectral-sailor.webp"
               alt="Spectral sailor"
+              width="auto"
+              height="auto"
+            />
+            <img
+              src="../assets/webp/soulkeeper.webp"
+              alt="Soulkeeper"
+              width="auto"
+              height="auto"
+            />
+            <img
+              src="../assets/webp/celestial-architect.webp"
+              alt="Celestial architect"
+              width="auto"
+              height="auto"
             />
           </div>
         </div>
@@ -278,11 +307,9 @@
   border-radius: var(--radius);
 }
 
-/****** Projects section ******/
+/***______ Projects Gallery ______***/
 .projects-section {
-  background-color: #242223;
-  margin: 10rem 0;
-  padding: 50px 0;
+  padding: var(--section-gap) 0;
 }
 
 .projects {
@@ -292,44 +319,77 @@
 .projects img {
   width: 100%;
   margin-bottom: 16px;
-  border-radius: var(--radius);
 }
 
-/****** More projects section ******/
-.more-projects {
-  position: relative;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin: 10rem 0;
+/***______ Project Showcase ______***/
+.project-showcase {
+  padding: var(--section-gap) 0;
 }
 
-.img-container {
-  width: 180px;
-  /* Largeur initiale du conteneur de l'image à 200px */
-  position: relative;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  overflow: hidden;
-  /* Cache les parties de l'image contenue qui dépassent (car l'image a 300px de largeur) */
+.project-showcase h2 {
+  text-align: center;
+  padding-bottom: 4rem;
+}
+
+.img-box {
+  width: 350px;
+  height: 140px;
+  margin: 3px;
   filter: grayscale(1);
-  transition: all 1s cubic-bezier(0.87, -0.5, 0.03, 1.54);
-  /* Crée une animation originale */
+  /* cache les parties de l'image contenue qui dépassent (car l'image a 300px de largeur) */
+  overflow: hidden;
+  /* crée une animation originale */
+  transition: all 0.6s cubic-bezier(0.87, -0.5, 0.03, 1.54);
   /* cubic-bezier(x1, y1, x2, y2); Si y2 > 1, l’animation dépasse sa cible puis revient (rebond).; Si y1 < 0, l’animation/image se rétrécit puis revient (rebond). */
-  margin: 0 3px;
+  /* •	0.87 : Indique une accélération initiale rapide.
+	•	-0.5 : Introduit une remontée brutale, simulant un rebond.
+	•	0.03 : Ralentit rapidement après le pic.
+	•	1.54 : Permet un effet de dépassement et de retour. */
 }
-
-.img-container img {
-  width: 525px;
-  height: 350px;
-  max-height: 525px;
-  object-fit: cover;
-}
-
-.img-container:hover {
-  /* Nouvelle largeur du conteneur de l'image au survol*/
-  width: 400px;
+.img-box:hover {
+  height: 255px;
   filter: grayscale(0);
+}
+
+.img-box img {
+  width: 400px;
+  object-fit: cover;
+  object-position: center;
+}
+
+/****** Media Queries ******/
+@media (width >= 576px) {
+  .img-box {
+    width: 500px;
+  }
+  .img-box:hover {
+    height: 380px;
+  }
+
+  .img-box img {
+    width: 600px;
+    height: auto;
+  }
+}
+
+@media (width >= 768px) {
+  .img-box {
+    width: 600px;
+  }
+}
+
+@media (width >= 992px) {
+  .img-box {
+    width: 180px;
+    height: 400px;
+  }
+  .img-box:hover {
+    width: 530px;
+    height: 400px;
+  }
+
+  .img-box img {
+    height: 400px;
+  }
 }
 </style>
