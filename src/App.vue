26fa3e4976d2b6164c929fa8bbb9ef93b67831a6
nicolasbilic <nicolasbilic9@gmail.com>
2025-01-26 16:57:35 +0100

    add loading animation back

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,46 @@
 <script setup lang="ts">
-// import { ref, onMounted } from "vue";
-// import LoadingScreen from "./components/Layout/LoadingScreen.vue";
+import { ref, onMounted } from "vue";
+import LoadingScreen from "./components/Layout/LoadingScreen.vue";
 
-// const isLoading = ref(true);
+const isLoading = ref(true);
 
-// // Récupère la date de la dernière visite et compare-la avec la date actuelle
-// const lastVisit = localStorage.getItem("lastVisit");
-// const now = new Date().getTime();
+// Récupère la date de la dernière visite et compare-la avec la date actuelle
+const lastVisit = localStorage.getItem("lastVisit");
+const now = new Date().getTime();
 
-// // Si l'user n'a jamais visité le site ou si la dernière visite remonte à plus de 1h, réinitialise le statut
-// if (!lastVisit || now - parseInt(lastVisit) > 60 * 60 * 1000) {
-//   localStorage.removeItem("hasVisited"); // Réinitialise "hasVisited"
-//   localStorage.setItem("lastVisit", now.toString()); // Met à jour la date de la dernière visite
-// } else {
-//   localStorage.setItem("lastVisit", now.toString()); // Met à jour la date de la dernière visite si l'user a visité le site dans les dernières 24h
-// }
+// Si l'user n'a jamais visité le site ou si la dernière visite remonte à plus de 1h, réinitialise le statut
+if (!lastVisit || now - parseInt(lastVisit) > 60 * 60 * 1000) {
+  localStorage.removeItem("hasVisited"); // Réinitialise "hasVisited"
+  localStorage.setItem("lastVisit", now.toString()); // Met à jour la date de la dernière visite
+} else {
+  localStorage.setItem("lastVisit", now.toString()); // Met à jour la date de la dernière visite si l'user a visité le site dans les dernières 24h
+}
 
-// onMounted(() => {
-//   const hasVisited = localStorage.getItem("hasVisited");
+onMounted(() => {
+  const hasVisited = localStorage.getItem("hasVisited");
 
-//   if (!hasVisited) {
-//     // Si c'est la première visite
-//     isLoading.value = true;
-//     localStorage.setItem("hasVisited", "true"); // Marque comme visité globalement
-//   } else {
-//     // Si déjà visité
-//     isLoading.value = false; // N'affiche pas l'écran de chargement
-//   }
-// });
+  if (!hasVisited) {
+    // Si c'est la première visite
+    isLoading.value = true;
+    localStorage.setItem("hasVisited", "true"); // Marque comme visité globalement
+  } else {
+    // Si déjà visité
+    isLoading.value = false; // N'affiche pas l'écran de chargement
+  }
+});
 
-// const handleFinishLoading = () => {
-//   isLoading.value = false;
-//   // l’écran de chargement (<LoadingScreen />) est masqué, car la directive v-if devient fausse.
-// };
+const handleFinishLoading = () => {
+  isLoading.value = false;
+  // l’écran de chargement (<LoadingScreen />) est masqué, car la directive v-if devient fausse.
+};
 </script>
 
 <template>
   <!-- L'événement finish-loading du composant LoadingScreen.vue peut être écouté par le parent App.vue  -->
-  <!-- <LoadingScreen v-if="isLoading" @finish-loading="handleFinishLoading" />
+  <LoadingScreen v-if="isLoading" @finish-loading="handleFinishLoading" />
   <div v-else>
     <RouterView />
-  </div> -->
-  <RouterView />
+  </div>
 </template>
 
 <style scoped></style>
